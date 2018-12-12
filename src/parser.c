954ebb8600e357a3423cbdaef1c763084b1eaff8
Tianhao Chai <cth451@gmail.com>
2018-12-12 15:41:27 -0600

    parser: handle integer and long integer arrays

--- src/parser.c
+++ src/parser.c
@@ -27,6 +27,89 @@ tag* parseFile(const char *buffer, const int length) {
 }
 
 /*
+ * Read in next payload w/ fixed length
+ *
+ * header:	Type of the payload
+ * dest:	Pointer to preallocated region write the payload
+ * buffer:	input buffer
+ * offset:	input offset
+ */
+void nextFixedLenPayload(tag_header header, void *dest, const char *buffer, int *offset) {
+	void *payload = dest;
+	if (header == TAG_BYTE) {
+		fprintf(stderr, "Found Byte %x\n", *(buffer + *offset));
+		*((char*) payload) = *(buffer + *offset);
+	} else if (header == TAG_SHORT) {
+		uint16_t s = be16toh(*((uint16_t *) (buffer + *offset)));
+		fprintf(stderr, "Found Short %i\n", s);
+		*((int16_t*) payload) = (int16_t) s;
+	} else if (header == TAG_INT) {
+		uint32_t s = be32toh(*((uint32_t *) (buffer + *offset)));
+		fprintf(stderr, "Found Int %i\n", s);
+		*((int32_t*) payload) = (int32_t) s;
+	} else if (header == TAG_LONG) {
+		uint64_t s = be64toh(*((uint64_t *) (buffer + *offset)));
+		fprintf(stderr, "Found Long %li\n", s);
+		*((int64_t*) payload) = (int64_t) s;
+	} else if (header == TAG_FLOAT) {
+		float s = (float) be32toh(*((uint32_t *) (buffer + *offset)));
+		fprintf(stderr, "Found Float %f\n", s);
+		*((float*) payload) = s;
+	} else if (header == TAG_DOUBLE) {
+		double s = (double) be64toh(*((uint64_t *) (buffer + *offset)));
+		fprintf(stderr, "Found Double %f\n", s);
+		*((double*) payload) = s;
+	}
+	*offset += type_length(header);
+}
+
+/*
+ * Read in next payload w/ variable length except for compound tags and list
+ * tags.  Returns 0 on success and -1 on failure.
+ *
+ * header:	Type of the payload
+ * size:	Pointer where the number of payload unit will be written
+ * 		i.e. String length, array sizes, list sizes
+ * dest:	Pointer to preallocated region write the payload
+ * buffer:	input buffer
+ * offset:	input offset
+ */
+int nextVarLenPayload(tag_header header, int32_t *size, void **dest,
+		const char *buffer, int *offset) {
+	void **payload = dest;
+	if (header == TAG_BYTES) {
+		// Read int size of payload in multiple of bytes
+		int32_t length = be32toh(*((uint32_t*) (buffer + *offset)));
+		*offset += 4;
+		*payload = p_malloc(length);
+		memcpy(*payload, buffer + *offset, length);
+		*offset += length;
+		*size = length;
+	} else if (header == TAG_STRING) {
+		*payload = nextString(buffer, offset);
+		*size = strlen((char *) *payload);
+	} else if (header == TAG_INTS || header == TAG_LONGS) {
+		// Read int size of payload in multiple of bytes
+		int32_t length = be32toh(*((uint32_t*) (buffer + *offset)));
+		*offset += 4;
+		int typeLen = type_length(header);
+		*payload = p_malloc(length * typeLen);
+		tag_header singleHdr = header - 8;
+		// Handle integers and longs by offloading to fixed length
+		for (int i = 0; i < length; i++) {
+			// Read i-th integer or long
+			nextFixedLenPayload(singleHdr, *payload + i * typeLen,
+					buffer, offset);
+		}
+		*size = length;
+	} else {
+		// Strange tags?
+		return -1;
+	}
+	return 0;
+}
+
+/*
  * Parse next tag
  *
  * buffer:	Entire tag mapped in memory
@@ -46,6 +129,13 @@ tag* nextTag(const char *buffer, int *offset) {
 		nbt = compoundDecomp(buffer, offset);
 		return nbt;
 	}
+	if (header == TAG_LIST) {
+		// Offloading this task to a function dedicated for lists
+		*offset -= 1;
+		// TODO
+		return nbt;
+	}
+
 	// Grab name first
 	char *name = nextString(buffer, offset);
 	nbt = p_malloc(sizeof(tag));
@@ -56,46 +146,17 @@ tag* nextTag(const char *buffer, int *offset) {
 		nbt -> size = 1;
 		nbt -> payload = p_malloc(type_length(header));
 		void *payload = nbt -> payload;
-		if (header == TAG_BYTE) {
-			fprintf(stderr, "Found Byte %x\n", *(buffer + *offset));
-			*((char*) payload) = *(buffer + *offset);
-		} else if (header == TAG_SHORT) {
-			uint16_t s = be16toh(*(buffer + *offset));
-			fprintf(stderr, "Found Short %i\n", s);
-			*((int16_t*) payload) = (int16_t) s;
-		} else if (header == TAG_INT) {
-			uint32_t s = be32toh(*(buffer + *offset));
-			fprintf(stderr, "Found Int %i\n", s);
-			*((int32_t*) payload) = (int32_t) s;
-		} else if (header == TAG_LONG) {
-			uint64_t s = be64toh(*(buffer + *offset));
-			fprintf(stderr, "Found Long %li\n", s);
-			*((int64_t*) payload) = (int64_t) s;
-		} else if (header == TAG_FLOAT) {
-			float s = (float) be32toh(*(buffer + *offset));
-			fprintf(stderr, "Found Float %f\n", s);
-			*((float*) payload) = s;
-		} else if (header == TAG_DOUBLE) {
-			double s = (double) be64toh(*(buffer + *offset));
-			fprintf(stderr, "Found Double %f\n", s);
-			*((double*) payload) = s;
-		}
-		*offset += type_length(header);
-	} else {
+		nextFixedLenPayload(header, payload, buffer, offset);
+	} else if (header < 13) {
+		// Tags with variable payload sizes
 		void *payload;
-		if (header == TAG_BYTES) {
-			// Read int size of payload in multiple of bytes
-			int32_t length = (int32_t) be32toh(*((uint32_t*) (buffer + *offset)));
-			*offset += 4;
-			payload = p_malloc(length);
-			memcpy(payload, buffer + *offset, length);
-			*offset += length;
-			nbt -> size = length;
-		} else if (header == TAG_STRING) {
-			payload = nextString(buffer, offset);
-			nbt -> size = strlen((char *) payload);
-		}
+		int32_t size;
+		nextVarLenPayload(header, &size, &payload, buffer, offset);
 		nbt -> payload = payload;
+		nbt -> size = size;
+	} else {
+		fprintf(stderr, "Unrecognized tag %02x at offset %i\n", header, *offset);
+		abort();
 	}
 	return nbt;
 }
