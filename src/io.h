36a9f4d1337abd20da6fc4a2b9a22ecc5991f1b9
Tianhao Chai <cth451@gmail.com>
2019-06-14 01:21:03 -0500

    cpp rewrite genesis

---
+++ src/io.h
@@ -0,0 +1,14 @@
+/* I/O helpers
+ *
+ */
+
+namespace nbtp {
+
+enum IOFormat {
+	TEXT = 0,
+	GZ = 1,
+	BIN = 2,
+	YAML = 3
+}
+
+}
--- src/tag.h
+++ src/tag.h
@@ -1,92 +1,90 @@
-#ifndef __libnbtp_tag
-#define __libnbtp_tag
-
-#include <stdio.h>
-#include <string.h>
-
-// The address to set when an error is encountered
-#define PARSE_ERR_PTR -1
-
-typedef char tag_header;
-
-// Not that size is the number of payload of appripriate type
-// i.e. size = 2 means 2 integers, etc.
-typedef struct tag {
-    tag_header header;
-    // If the tag is part of a list, the name will be NULL
-    char *name;
-    int size;
-    // Only used when this tag is a list
-    tag_header listType;
-    /*
-     * If the tag is
-     * - compound
-     *     payload points to an array of tag*
-     * - list
-     *     payload points to content of the list formed into tags of same type
-     * - int(s), long(s), byte(s)
-     *     payload points to an array of ints, longs, or bytes.
-     *     Note that this array can be of size 1.
-     * - string
-     *     payload is a char*.
-     */
-    void *payload;
-} tag;
-
-/*
- * Tag header byte to tell different tags apart
- */
-
-#define TAG_END         0
-#define TAG_BYTE        1
-#define TAG_SHORT       2
-#define TAG_INT         3
-#define TAG_LONG        4
-#define TAG_FLOAT       5
-#define TAG_DOUBLE      6
-#define TAG_BYTES       7
-#define TAG_STRING      8
-#define TAG_LIST        9
-#define TAG_COMPOUND    10
-#define TAG_INTS        11
-#define TAG_LONGS       12
-
-/* Return whether this type can have variable payload size */
-int tag_variable_length(tag_header header);
-
-/* Return the unit payload length */
-int type_length(tag_header header);
-
-
-/* Calculates the size of tags if written as nbt binary
+/* Tag syntax tree nodes
  *
- * nbt:     Tag to be calculated
  */
-int nbtp_bin_size(tag* nbt);
+
+#include "io.h"
+
+#include <cstdint>
+
+namespace nbtp {
+
+class Tag {
+public:
+	std::string name;
+
+	virtual void output(IOFormat fmt);
+	virtual void output()
+	{
+		output(IOFormat::TEXT);
+	}
+}
 
 /*
- * Deallocate tags on heap
+ * These tags has a fixed length
  */
-void nbtp_free_tag(tag* nbt);
+class ByteTag : public Tag {
+public:
+	int8_t value;
+}
+
+class ShortTag : public Tag {
+public:
+	int16_t value;
+}
+
+class IntTag : public Tag {
+public:
+	int32_t value;
+}
+
+class LongTag : public Tag {
+pubic:
+	int64_t value;
+}
+
+class FloatTag : public Tag {
+public:
+	float value;
+}
+
+class DoubleTag : public Tag {
+public:
+	double value;
+}
+
+// These tags may contain dynamic payload sizes
 
 /*
- * Recursive print
- *
- * nbt:		The tag to print
- * f:		The FILE to output
- * indent:	Indentation of output
- * columns:	The row width of array outputs, 0 to use default 32.
+ * A prototype DynamicTag, serving as the basis for all other tags.
  */
-void nbtp_print_tag(tag *nbt, FILE *f, int indent, int columns);
+class DynamicTag : public Tag {
+public:
+	std::vector<Tag> payload;
+}
 
 /*
- * Write out a tag in binary
- *
- * nbt:		The tag to print
- * f:		The FILE to write to
+ * List and compound tags share largely the same data structure. The only difference is that
+ * compound tags store full tags, but lists store payloads only. Here I'm using lists as a
+ * "special" situation where __names__ of subtags are not important.
  */
-void nbtp_write_tag(tag *nbt, FILE *f);
+class CompoundTag : public DynamicTag {
+}
 
-#endif
+class ListTag : public Compound {
+}
 
-// vim: set tabstop=4 expandtab :
+/*
+ * The rest of variable length tags are constructured as special cases of ListTag
+ */
+class ByteArrayTag : public ListTag {
+}
+
+class StringTag : public ByteArrayTag {
+	// Note that String in Minecraft is essentially the same as a byte array
+}
+
+class IntArrayTag : public ListTag {
+}
+
+class LongArray : public ListTag {
+}
