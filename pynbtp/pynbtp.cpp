28a5e4395434d62142c86d9f327a1894c81e6b31
Tianhao Chai <cth451@gmail.com>
2020-05-14 00:28:09 -0500

    cleanup

--- pynbtp/pynbtp.cpp
+++ pynbtp/pynbtp.cpp
@@ -52,7 +52,7 @@ namespace pyNBTP {
 }
 
 PYBIND11_MODULE(pynbtp, m) {
-
+	m.doc() = "Python bindings for libnbtp";
 
 	// Enums
 	{
@@ -76,83 +76,80 @@ PYBIND11_MODULE(pynbtp, m) {
 				.value("PRETTY_PRINT", IOFormat::PRETTY_PRINT);
 	}
 
+	// Classes
 	{
 		using namespace NBTP;
+		using namespace std;
 		// Virtual Classes
 		py::class_<Tag, std::shared_ptr<Tag>>(m, "Tag")
 				.def("output", pyNBTP::pyOutputTag)
 				.def("typeCode", &Tag::typeCode)
 				.def("__repr__", &Tag::toString);
-		py::class_<SingleValuedTag<int8_t>, Tag, std::shared_ptr<SingleValuedTag<int8_t>>>(m, "__SingleValuedInt8Tag");
-		py::class_<SingleValuedTag<int16_t>, Tag, std::shared_ptr<SingleValuedTag<int16_t>>>(m,
-		                                                                                     "__SingleValuedInt16Tag");
-		py::class_<SingleValuedTag<int32_t>, Tag, std::shared_ptr<SingleValuedTag<int32_t>>>(m,
-		                                                                                     "__SingleValuedInt32Tag");
-		py::class_<SingleValuedTag<int64_t>, Tag, std::shared_ptr<SingleValuedTag<int64_t>>>(m,
-		                                                                                     "__SingleValuedInt64Tag");
-		py::class_<SingleValuedTag<float>, Tag, std::shared_ptr<SingleValuedTag<float>>>(m, "__SingleValuedFloatTag");
-		py::class_<SingleValuedTag<double>, Tag, std::shared_ptr<SingleValuedTag<double>>>(m,
-		                                                                                   "__SingleValuedDoubleTag");
-		py::class_<ContainerTag, Tag, std::shared_ptr<ContainerTag>>(m, "ContainerTag")
+		py::class_<SingleValuedTag<int8_t>, Tag, shared_ptr<SingleValuedTag<int8_t>>>(m, "__SingleValuedInt8Tag");
+		py::class_<SingleValuedTag<int16_t>, Tag, shared_ptr<SingleValuedTag<int16_t>>>(m, "__SingleValuedInt16Tag");
+		py::class_<SingleValuedTag<int32_t>, Tag, shared_ptr<SingleValuedTag<int32_t>>>(m, "__SingleValuedInt32Tag");
+		py::class_<SingleValuedTag<int64_t>, Tag, shared_ptr<SingleValuedTag<int64_t>>>(m, "__SingleValuedInt64Tag");
+		py::class_<SingleValuedTag<float>, Tag, shared_ptr<SingleValuedTag<float>>>(m, "__SingleValuedFloatTag");
+		py::class_<SingleValuedTag<double>, Tag, shared_ptr<SingleValuedTag<double>>>(m, "__SingleValuedDoubleTag");
+		py::class_<ContainerTag, Tag, shared_ptr<ContainerTag>>(m, "ContainerTag")
 				.def("size", &ContainerTag::size);
 
 		// Base classes
-		py::class_<ListTag, ContainerTag, Tag, std::shared_ptr<ListTag>>(m, "ListTag")
+		py::class_<ListTag, ContainerTag, Tag, shared_ptr<ListTag>>(m, "ListTag")
 				.def(py::init<>())
 				.def("insert", &ListTag::insert)
 				.def("setContentType", &ListTag::setContentType)
 				.def("getContentType", &ListTag::getContentType)
 				.def("getPayload", &ListTag::getPayload);
-		py::class_<CompoundTag, ContainerTag, Tag, std::shared_ptr<CompoundTag>>(m, "CompoundTag")
+		py::class_<CompoundTag, ContainerTag, Tag, shared_ptr<CompoundTag>>(m, "CompoundTag")
 				.def(py::init<>())
 				.def("insert", &CompoundTag::insert)
 				.def("lookup", &CompoundTag::lookup)
 				.def("getPayload", &CompoundTag::getPayload);
-		py::class_<StringTag, ContainerTag, Tag, std::shared_ptr<StringTag>>(m, "StringTag")
+		py::class_<StringTag, ContainerTag, Tag, shared_ptr<StringTag>>(m, "StringTag")
 				.def(py::init<>())
 				.def(py::init<std::string &>())
 				.def("setPayload", &StringTag::setPayload)
 				.def("getPayload", &StringTag::getPayload);
 
 		// Single Valued Tags
-		py::class_<ByteTag, SingleValuedTag<int8_t>, Tag, std::shared_ptr<ByteTag>>(m, "ByteTag")
+		py::class_<ByteTag, SingleValuedTag<int8_t>, Tag, shared_ptr<ByteTag>>(m, "ByteTag")
 				.def(py::init<int8_t>())
 				.def("setPayload", &ByteTag::setPayload)
 				.def("getPayload", &ByteTag::getPayload);
-		py::class_<ShortTag, SingleValuedTag<int16_t>, Tag, std::shared_ptr<ShortTag>>(m, "ShortTag")
+		py::class_<ShortTag, SingleValuedTag<int16_t>, Tag, shared_ptr<ShortTag>>(m, "ShortTag")
 				.def(py::init<int16_t>())
 				.def("setPayload", &ShortTag::setPayload)
 				.def("getPayload", &ShortTag::getPayload);
-		py::class_<IntTag, SingleValuedTag<int32_t>, Tag, std::shared_ptr<IntTag>>(m, "IntTag")
+		py::class_<IntTag, SingleValuedTag<int32_t>, Tag, shared_ptr<IntTag>>(m, "IntTag")
 				.def(py::init<int32_t>())
 				.def("setPayload", &IntTag::setPayload)
 				.def("getPayload", &IntTag::getPayload);
-		py::class_<LongTag, SingleValuedTag<int64_t>, Tag, std::shared_ptr<LongTag>>(m, "LongTag")
+		py::class_<LongTag, SingleValuedTag<int64_t>, Tag, shared_ptr<LongTag>>(m, "LongTag")
 				.def(py::init<int64_t>())
 				.def("setPayload", &LongTag::setPayload)
 				.def("getPayload", &LongTag::getPayload);
-		py::class_<FloatTag, SingleValuedTag<float>, Tag, std::shared_ptr<FloatTag>>(m, "FloatTag")
+		py::class_<FloatTag, SingleValuedTag<float>, Tag, shared_ptr<FloatTag>>(m, "FloatTag")
 				.def(py::init<float>())
 				.def("setPayload", &FloatTag::setPayload)
 				.def("getPayload", &FloatTag::getPayload);
-		py::class_<DoubleTag, SingleValuedTag<double>, Tag, std::shared_ptr<DoubleTag>>(m, "DoubleTag")
+		py::class_<DoubleTag, SingleValuedTag<double>, Tag, shared_ptr<DoubleTag>>(m, "DoubleTag")
 				.def(py::init<double>())
 				.def("setPayload", &DoubleTag::setPayload)
 				.def("getPayload", &DoubleTag::getPayload);
 
 		// Types that extends ListTag
-		py::class_<BytesTag, ListTag, ContainerTag, Tag, std::shared_ptr<BytesTag>>(m, "BytesTag");
-		py::class_<IntsTag, ListTag, ContainerTag, Tag, std::shared_ptr<IntsTag>>(m, "IntsTag");
-		py::class_<LongsTag, ListTag, ContainerTag, Tag, std::shared_ptr<LongsTag>>(m, "LongsTag");
+		py::class_<BytesTag, ListTag, ContainerTag, Tag, shared_ptr<BytesTag>>(m, "BytesTag");
+		py::class_<IntsTag, ListTag, ContainerTag, Tag, shared_ptr<IntsTag>>(m, "IntsTag");
+		py::class_<LongsTag, ListTag, ContainerTag, Tag, shared_ptr<LongsTag>>(m, "LongsTag");
 	}
 
 	// Parsing entry point
 	{
-		using namespace NBTP;
 		m.def("parseRoot", &pyNBTP::pyParseRoot , "Parse Root Tag");
 		m.def("parseRoot", &pyNBTP::pyParseRootFormat, "Parse Root Tag");
 		m.def("writeRoot", &pyNBTP::pyWriteRoot, "Write Root Tag");
 	}
 
-	m.def("mksimple", &pyNBTP::mksimple, "A function which adds two numbers");
+	m.def("mksimple", &pyNBTP::mksimple, "A function to create a simple compound tag");
 }
