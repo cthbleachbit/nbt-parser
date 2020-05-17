af01e5800509b55a75cf680de8d6399e67041268
Tianhao Chai <cth451@gmail.com>
2020-05-17 01:54:22 -0500

    assume windows runs on little endian

--- src/Conversion.cpp
+++ src/Conversion.cpp
@@ -14,24 +14,20 @@
 #include <endian.h>
 #else
 #include <winsock2.h>
-#include <sys/param.h>
-#if BYTE_ORDER == LITTLE_ENDIAN
+#define htobe16(x) htons(x)
+#define htole16(x) (x)
+#define be16toh(x) ntohs(x)
+#define le16toh(x) (x)
 
-#	define htobe16(x) htons(x)
-#	define htole16(x) (x)
-#	define be16toh(x) ntohs(x)
-#	define le16toh(x) (x)
+#define htobe32(x) htonl(x)
+#define htole32(x) (x)
+#define be32toh(x) ntohl(x)
+#define le32toh(x) (x)
 
-#	define htobe32(x) htonl(x)
-#	define htole32(x) (x)
-#	define be32toh(x) ntohl(x)
-#	define le32toh(x) (x)
-
-#	define htobe64(x) htonll(x)
-#	define htole64(x) (x)
-#	define be64toh(x) ntohll(x)
-#	define le64toh(x) (x)
-#endif
+#define htobe64(x) htonll(x)
+#define htole64(x) (x)
+#define be64toh(x) ntohll(x)
+#define le64toh(x) (x)
 #endif
 
 
