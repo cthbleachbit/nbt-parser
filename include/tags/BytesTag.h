0ffc02cc5e8388371240609ddb66707a53327410
Tianhao Chai <cth451@gmail.com>
2020-12-18 21:32:16 -0600

    BytesTag: add copy and move

--- include/tags/BytesTag.h
+++ include/tags/BytesTag.h
@@ -48,6 +48,14 @@ namespace NBTP {
 		BytesTag(std::istream &input, ssize_t &counter, IOFormat format);
 
 		/**
+		 * Copy constructor
+		 * @param tag  byte array tag to copy from
+		 */
+		BytesTag(const BytesTag &tag) noexcept = default;
+
+		BytesTag(BytesTag &&tag) noexcept = default;
+
+		/**
 		 * Trivial constructor
 		 */
 		BytesTag() = default;
