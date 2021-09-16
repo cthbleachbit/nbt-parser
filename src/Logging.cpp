898c9add398be37711356e6142c205f6f96aa41c
Tianhao Chai <cth451@gmail.com>
2021-09-16 14:41:29 -0500

    logging: print an endl after warning

--- src/Logging.cpp
+++ src/Logging.cpp
@@ -8,7 +8,7 @@
 
 namespace NBTP {
 	void Logging::warn(const std::string &msg, ssize_t counter) {
-		fprintf(stderr, "Warning at byte offset %li: %s", counter, msg.c_str());
+		fprintf(stderr, "Warning at byte offset %li: %s\n", counter, msg.c_str());
 	}
 
 	void Logging::error(const std::string &msg, ssize_t counter) {
