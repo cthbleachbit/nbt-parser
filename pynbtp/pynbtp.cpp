b859c68483287ef7d312bffbd8d7ca60797c53ec
Tianhao Chai <cth451@gmail.com>
2022-11-07 21:05:29 -0500

    pynbtp: even more commentary

--- pynbtp/pynbtp.cpp
+++ pynbtp/pynbtp.cpp
@@ -1,6 +1,6 @@
-//
-// Created by cth451 on 2020/05/12.
-//
+/**
+ * @file pynbtp bindings - main module definitions
+ */
 
 #include <pybind11/pybind11.h>
 #include <pybind11/iostream.h>
@@ -14,12 +14,12 @@ namespace py = pybind11;
 namespace pyNBTP {
 	using namespace NBTP;
 
-	std::shared_ptr<CompoundTag> mksimple() {
-		auto tag = std::make_shared<CompoundTag>();
-		tag->insert("tag1", std::make_shared<IntTag>(14));
-		return tag;
-	}
-
+	/**
+	 * write tag with specified format
+	 * @param tag     tag to write
+	 * @param io      python output stream
+	 * @param format  format specifier
+	 */
 	void pyOutputTag(Tag *tag, pybind11::object &io, IOFormat format) {
 		if (format == PRETTY_PRINT) {
 			pybind11::detail::pythonbuf buffer(io);
@@ -32,6 +32,11 @@ namespace pyNBTP {
 		}
 	}
 
+	/**
+	 * Parse a tag that is read from a python binary buffer
+	 * @param io   python binary input stream
+	 * @return a Tag object or None if parsing fails
+	 */
 	std::shared_ptr<Tag> pyParseRoot(pybind11::object &io) {
 		ssize_t counter;
 		PyIBytesBuf buffer(io);
@@ -39,13 +44,26 @@ namespace pyNBTP {
 		return TagIO::parseRoot(in, counter);
 	}
 
-	pybind11::object pyParseRootFormat(pybind11::object &io, IOFormat format) {
+	/**
+	 * Parse a tag in specified format
+	 * Only binary is supported at this time. Other formats are not implemented.
+	 *
+	 * @param io              python input stream
+	 * @param format          format specifier
+	 * @return a Tag object or None if parsing fails
+	 */
+	std::shared_ptr<Tag> pyParseRootFormat(pybind11::object &io, IOFormat format) {
 		ssize_t counter;
 		PyIBytesBuf buffer(io);
 		std::istream in(&buffer);
-		return pybind11::make_tuple(TagIO::parseRoot(in, counter, format), counter);
+		return TagIO::parseRoot(in, counter, format);
 	}
 
+	/**
+	 * Write out an in-memory tag in binary format
+	 * @param io      python binary output stream
+	 * @param tag     tag to write
+	 */
 	void pyWriteRoot(pybind11::object &io, Tag &tag) {
 		PyOBytesBuf buffer(io);
 		std::ostream out(&buffer);
@@ -53,32 +71,35 @@ namespace pyNBTP {
 	}
 }
 
+/**
+ * pynbtp Module definition
+ */
 PYBIND11_MODULE(pynbtp, m) {
 	m.doc() = "Python bindings for libnbtp";
 
-	// Enums
+	/* Enums */
 	{
 		using namespace NBTP;
-		py::enum_<TagType>(m, "TagType")
-				.value("BYTE", TagType::BYTE)
-				.value("SHORT", TagType::SHORT)
-				.value("INT", TagType::INT)
-				.value("LONG", TagType::LONG)
-				.value("FLOAT", TagType::FLOAT)
-				.value("DOUBLE", TagType::DOUBLE)
-				.value("LIST", TagType::LIST)
-				.value("COMPOUND", TagType::COMPOUND)
-				.value("STRING", TagType::STRING)
-				.value("BYTES", TagType::BYTES)
-				.value("INTS", TagType::INTS)
-				.value("LONGS", TagType::LONGS)
-				.value("END", TagType::END);
-		py::enum_<IOFormat>(m, "IOFormat")
-				.value("BIN", IOFormat::BIN)
-				.value("PRETTY_PRINT", IOFormat::PRETTY_PRINT);
+		py::enum_<TagType>(m, "TagType", "Possible payload type for NBT tags")
+				.value("BYTE", TagType::BYTE, "an 8-bit signed integer")
+				.value("SHORT", TagType::SHORT, "a 16-bit signed integer")
+				.value("INT", TagType::INT, "a 32-bit signed integer")
+				.value("LONG", TagType::LONG, "a 64-bit signed integer")
+				.value("FLOAT", TagType::FLOAT, "a 32-bit IEEE-754 floating point decimal")
+				.value("DOUBLE", TagType::DOUBLE, "a 64-bit IEEE-754 floating point decimal")
+				.value("LIST", TagType::LIST, "a list of tags of the same type")
+				.value("COMPOUND", TagType::COMPOUND, "a set of named tags")
+				.value("STRING", TagType::STRING, "a string literal")
+				.value("BYTES", TagType::BYTES, "a list of 8-bit signed integers")
+				.value("INTS", TagType::INTS, "a list of 32-bit signed integers")
+				.value("LONGS", TagType::LONGS, "a list of 64-bit signed integers")
+				.value("END", TagType::END, "an empty tag - used only for type designation for empty lists");
+		py::enum_<IOFormat>(m, "IOFormat", "Supported I/O format when handling tags")
+				.value("BIN", IOFormat::BIN, "Uncompressed NBT in binary")
+				.value("PRETTY_PRINT", IOFormat::PRETTY_PRINT, "Human readable text - as tag output only");
 	}
 
-	// Classes
+	/* Classes */
 	{
 		using namespace NBTP;
 		using namespace std;
@@ -96,10 +117,19 @@ PYBIND11_MODULE(pynbtp, m) {
 		py::class_<SingleValuedTag<int64_t>, Tag, shared_ptr<SingleValuedTag<int64_t>>>(m, "__SingleValuedInt64Tag");
 		py::class_<SingleValuedTag<float>, Tag, shared_ptr<SingleValuedTag<float>>>(m, "__SingleValuedFloatTag");
 		py::class_<SingleValuedTag<double>, Tag, shared_ptr<SingleValuedTag<double>>>(m, "__SingleValuedDoubleTag");
-		py::class_<ContainerTag, Tag, shared_ptr<ContainerTag>>(m, "ContainerTag")
-				.def("size", &ContainerTag::size);
+		py::class_<ContainerTag, Tag, shared_ptr<ContainerTag>>
+				(
+						m,
+						"ContainerTag",
+						"Abstract class encompassing tags that may contain other tags"
+				)
+				.def(
+						"size",
+						&ContainerTag::size,
+						"Number of elements in the container tag"
+				);
 
-		// Base classes
+		/* Base classes */
 		py::class_<ListTag, ContainerTag, Tag, shared_ptr<ListTag>>
 				(
 						m,
@@ -132,7 +162,7 @@ PYBIND11_MODULE(pynbtp, m) {
 				(
 						m,
 						"CompoundTag",
-						"A container tag that holds a mapping from names to tags (potentially of different types)"
+						"A container tag that holds a set of named tags (potentially of different types)"
 				)
 				.def(py::init<>())
 				.def(
@@ -162,7 +192,7 @@ PYBIND11_MODULE(pynbtp, m) {
 				.def("setPayload", &StringTag::setPayload)
 				.def("getPayload", &StringTag::getPayload);
 
-		// Single Valued Tags
+		/* Single Valued Tags */
 		py::class_<ByteTag, SingleValuedTag<int8_t>, Tag, shared_ptr<ByteTag>>
 				(
 						m,
@@ -218,7 +248,7 @@ PYBIND11_MODULE(pynbtp, m) {
 				.def("setPayload", &DoubleTag::setPayload)
 				.def("getPayload", &DoubleTag::getPayload);
 
-		// Types that extends ListTag
+		/* Types that extends ListTag */
 		py::class_<BytesTag, ListTag, ContainerTag, Tag, shared_ptr<BytesTag>>
 				(
 						m,
@@ -239,7 +269,7 @@ PYBIND11_MODULE(pynbtp, m) {
 				);
 	}
 
-	// Parsing entry point
+	/* Parsing / binary output entry point */
 	{
 		m.def("parseRoot", &pyNBTP::pyParseRoot, "Parse a root tag from a binary input stream");
 		// m.def("parseRoot", &pyNBTP::pyParseRootFormat, "Parse Root Tag");
